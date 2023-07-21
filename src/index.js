366a3327364910bfb927c18ad56cd2331c0c184c
Anas5006 <anassain13@gmail.com>
2023-07-21 11:32:25 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
-const dotenv = require('dotenv')
+const env = require('dotenv')
 const mongoose = require('mongoose')
 const user = require('./routes/user')
 const place = require('./routes/place')
@@ -9,15 +9,13 @@ const booking = require('./routes/booking')
 const helmet = require("helmet");
 
 const app = express()
-const http = require("http");
-
-dotenv.config();
+env.config();
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(helmet())
 
-const server = http.createServer(app);
+
 
 app.use(cors({
     origin: 'https://air-bnb-client.vercel.app/', 
@@ -27,13 +25,11 @@ app.use(cors({
     maxAge: 5000,
     exposedHeaders: ['*', 'Authorization' ]
 }))
-mongoose.connect(process.env.MONGO_URI,{
+mongoose.connect(process.env.MONGODB_CONNECTION,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 }).then(()=>{
     console.log("DataBase Connected")
-}).catch((err) =>{
-    console.log("DataBase Connection Failed" + err)
 })
 app.get('/', (req,res)=>{
     return res.json({message:"App is Running"})
@@ -42,6 +38,6 @@ app.use('/api/v1/user',user)
 app.use('/api/v1/place',place)
 app.use('/api/v1/booking',booking)
 
-server.listen(4000, ()=>{
+app.listen(4000, ()=>{
     console.log('Server is Running on Port 4000')
 })
